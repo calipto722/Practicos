6a2202734c556f53191e57b69c9f10ff25052aa8
calipto722 <141640050+calipto722@users.noreply.github.com>
2024-02-16 15:00:41 -0300

    ultimo

---
+++ javascrip/tp_A/ejercicio_5/ejercicio_6/codigo.js
@@ -0,0 +1,12 @@
+/*6) Leer una cadena de texto desde un prompt y generar un array con la función split().
+Posteriormente, mostrar la siguiente información: Número de palabras, primera palabra,
+última palabra, las palabras colocadas en orden inverso, las palabras ordenadas de la “a”
+hasta la “z” y las palabras ordenadas de la “z” hasta la ”a”*/
+var cadena = prompt("ingresar una cadena");
+var separador =' '
+var areglo_cadena = cadena.split(separador); 
+console.log("arreglo " + areglo_cadena)
+console.log('cantidad de palabras en el arreglo : ' + areglo_cadena.length);
+console.log(' primer palabra en el arreglo : ' + areglo_cadena[0]);
+console.log('ultima palabra del arreglo : ' + areglo_cadena[areglo_cadena.length - 1]);
+
